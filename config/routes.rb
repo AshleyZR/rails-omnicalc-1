e0d38f138ed142f391a0757b9c4e59adac2ba8ba
Ziyu (Ashley) Ren <92663054+AshleyZR@users.noreply.github.com>
2023-10-31 21:55:24 +0000

    finished omicalc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller => "omicalc", :action => "home"})
+  get("/square/new", {:controller => "omicalc", :action => "square_new"})
+  get("/square/results", {:controller => "omicalc", :action => "square_results"})
+  get("/square_root/new", {:controller => "omicalc", :action => "square_root_new"})
+  get("/square_root/results", {:controller => "omicalc", :action => "square_root_results"})
+  get("/payment/new", {:controller => "omicalc", :action => "payment_new"})
+  get("/payment/results", {:controller => "omicalc", :action => "payment_results"})
+  get("/random/new", {:controller => "omicalc", :action => "random_new"})
+  get("/random/results", {:controller => "omicalc", :action => "random_results"})
 end
